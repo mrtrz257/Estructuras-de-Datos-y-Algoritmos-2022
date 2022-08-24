173d3f2da9e80452577c689f28e3317a7828d933
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-08-24 17:30:06 -0300

    Add files via upload

---
+++ Ejercicio 4/Pila.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+class Pila:
+    __arreglo = None
+    __cant = None
+    __tope1 = None
+    __tope2 = None
+    def __init__(self, cantidad = 0):
+        self.__arreglo = np.empty(self.__cant, dtype=int)
+        self.__cant = cantidad
+        self.__tope1 = -1
+        self.__tope2 = cantidad+1
+    def vacia1(self):
+        return self.__tope1==-1
+    def vacia2(self):
+        return self.__tope2==self.__cant+1
+    def insertar1(self, x):
+        if self.__tope1<self.__tope2-1:
+            self.__tope1 += 1
+            self.__arreglo[self.__tope1] = x
+            return x
+        else:
+            return 0
+    def insertar2(self, x):
+        if self.__tope2>self.__tope1+1:
+            self.__tope2 -= -1
+            self.__arreglo[self.__tope2] = x
+            return x
+        else:
+            return 0
+    def suprimir1(self):
+        if self.vacia1():
+            print("Pila Vacia")
+            return 0
+        else:
+            self.__tope1 -= 1
+            x = self.__arreglo[self.__tope1]
+            return x
+    def suprimir2(self):
+        if self.vacia2():
+            print("Pila Vacia")
+            return 0
+        else:
+            x = self.__arreglo[self.__tope2]
+            self.__tope2 += 1
+            return x
+    def mostrar1(self):
+        if not self.vacia1():
+            i = 0
+            tope = self.__tope1
+            while i <= tope:
+                elemento = self.suprimir1()
+                if elemento != -1:
+                    print(elemento)
+                i += 1
+    def mostrar2(self):
+        if not self.vacia2():
+            i = self.__cant - 1
+            tope = self.__tope2
+            while i >= tope:
+                elemento = self.suprimir2()
+                if elemento != -1:
+                    print(elemento)
+                i -= 1
---
+++ Ejercicio 4/main.py
@@ -0,0 +1,22 @@
+from Pila import Pila
+
+def Ejercicio4():
+    pila = Pila(8)
+    band = True
+    ver = -1
+    while band:
+        if
+            num = input("Ingrese Numero: ")
+            pil = input("Ingrese Pila: ")
+            if pil == "1":
+                if pila.insertar1(num)==0:
+                    print("Pila llena")
+            elif pil == "2":
+                if pila.insertar2(num)==0:
+                    print("Pila llena")
+            else:
+                print("Pila no existe")
+
+
+if __name__ == '__main__':
+    Ejercicio4()
