6c15847b5449a109aa676d41de8122f6cf87ba63
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-09-19 17:14:25 -0300

    Delete Arbol/Ejercicio 1 directory

--- Arbol/Ejercicio 1/main.py
+++
@@ -1,9 +0,0 @@
-from arbolBB import arbolBB
-
-if __name__ == '__main__':
-    arbol = arbolBB()
-    arbol.insertarRaiz(4)
-    arbol.insertar(6)
-    arbol.insertar(1)
-    print(arbol.buscar(1))
-    print(arbol.mostrarRaiz())
