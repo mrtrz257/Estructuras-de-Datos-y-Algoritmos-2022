9a0900401a29ff9b463ed239e3152f889c3e7cea
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-09-30 16:46:33 -0300

    Update main.py

--- Arbol/Ejercicio 3/main.py
+++ Arbol/Ejercicio 3/main.py
@@ -10,20 +10,6 @@ if __name__ == '__main__':
     arbol.insertar(5)
     arbol.insertar(10)
     arbol.insertar(3)
-    #print("Buscar 7 en el arbol", arbol.buscar(7))
-    #print("Buscar 10 en el arbol", arbol.buscar(10))
     print("Altura del arbol: ", arbol.altura())
     print("Cantidad de Nodos en el arbol: ", arbol.contador())
-    print("------------------")
-    '''print("Nivel de 9:", arbol.nivel(9))
-    print("Nivel de 15:", arbol.nivel(15))
-    print("4 es hoja?", arbol.hoja(4))
-    print("5 es hoja?", arbol.hoja(5))
-    print("4 padre de 2:", arbol.padre(4,2))
-    print("10 padre de 5:", arbol.padre(10,5))
-    print("2 hijo de 4:", arbol.hijo(2,4))
-    print("5 hijo de 10:", arbol.hijo(5,10))
-    print("Frontera del arbol:")'''
-    #arbol.frontera()
-
-
+    
