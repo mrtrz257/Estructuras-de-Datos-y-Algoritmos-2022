7c4d266cdbfd0df1a0a5c1085aebaff1ef851349
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-10-22 18:16:05 -0300

    Add files via upload

--- Hashing/Ejercicio 2/main.py
+++ Hashing/Ejercicio 2/main.py
@@ -1,12 +1,19 @@
 import random
-
 from Hashing import DirecAbierto
 
 if __name__ == '__main__':
     tabla = DirecAbierto()
-    pseudoRand = int(random.uniform(1, 6))
-    numeroClaves = 1000
+    pseudoRand = tabla.GenerarPseudoRandom()
+    numeroClaves = 8.4
     tabla.crear(numeroClaves)
-    for i in range(numeroClaves):
-        clave = int(random.uniform(0, 2000))
-        tabla.insertar(clave, pseudoRand, numeroClaves)
+    tamanio = tabla.CalcularTamanioTabla(numeroClaves)
+    print("-_________________________________-")
+    '''for i in range(numeroClaves):
+        clave = int(random.uniform(0, 1000))
+        tabla.insertar(clave, pseudoRand, tamanio)
+    clv = int(input("Ingresar para buscar: "))
+    busq = tabla.buscar(clv, tamanio)
+    if busq == -1:
+        print("No se encontro la clave")
+    else:
+        print("Se encontro la clave {}".format(clv))'''
